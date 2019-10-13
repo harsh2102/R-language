4e94f49331bc34232bd565613cef6f069de1cc74
sonuc9918 <56512331+sonuc9918@users.noreply.github.com>
2019-10-13 20:42:28 +0530

    update comments

--- sum(input from user).R
+++ sum(input from user).R
@@ -2,7 +2,7 @@
 num1<- as.integer(readline(prompt = "enter 1st no."))
 num2<- as.integer(readline(prompt = "enter 2nd no."))
 
-
+#calculating the sum
 sum<- num1 + num2
-
+#printing the sum
 print(paste("the sum of ", num1 ," and ",num2," is = ",sum))
